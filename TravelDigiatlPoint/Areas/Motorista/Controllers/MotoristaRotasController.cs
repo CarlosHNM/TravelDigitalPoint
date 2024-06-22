f92e1525c571b26623f88b44ac7163500246bf49
Carlos Henrique <31441314+CarlosHNM@users.noreply.github.com>
2024-06-22 11:15:06 -0300

    Criação do Projeto
    Modificação de alguns metodos que não estava funcionando adequadamente

--- TravelDigiatlPoint/Areas/Motorista/Controllers/MotoristaRotasController.cs
+++ TravelDigiatlPoint/Areas/Motorista/Controllers/MotoristaRotasController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TravelDigiatlPoint.ViewModel;
 using TravelDigiatlPoint.Context;
+using TravelDigiatlPoint.Models;
 using TravelDigiatlPoint.Repositories.Interface;
+using TravelDigiatlPoint.ViewModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TravelDigiatlPoint.Areas.Motorista.Controllers
 {
@@ -20,8 +23,123 @@ namespace TravelDigiatlPoint.Areas.Motorista.Controllers
 
         public IActionResult Index()
         {
-            var Funcionario = new FuncionarioViewModel { };
-            return View(Funcionario);
+            var viewModel = new FuncionarioViewModel
+            {
+                Funcionarios = _context.Funcionarios.Where(x => x.Cargo == Cargo.Motorista).ToList()
+            };
+            return View(viewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var rotas = _rotasTrabalhoRepository.Rotas
+                .Where(r => r.FuncionarioId == id)
+                .ToList();
+
+            if (!rotas.Any())
+            {
+                return NotFound();
+            }
+
+            return View(rotas);
+        }
+
+        // GET: MotoristaRotas/StartTrip/5
+        public async Task<IActionResult> StartTrip(int id)
+        {
+            var rota = await _context.Rotas
+                .Include(r => r.Veiculo)
+                .Include(r => r.Funcionario)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (rota == null)
+            {
+                return NotFound();
+            }
+
+            rota.DataHoraPartida = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rota);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Rotas.Any(e => e.Id == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return RedirectToAction("Details", new { id = rota.FuncionarioId });
+        }
+
+
+        // GET: MotoristaRota/EndTrip/5
+        public async Task<IActionResult> EndTrip(int id)
+        {
+            var rota = await _context.Rotas
+                .Include(r => r.Veiculo)
+                .Include(r => r.Funcionario)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (rota == null)
+            {
+                return NotFound();
+            }
+
+            return View(rota);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EndTrip(int id, [Bind("Id,DataHoraChegada,FlDanos,ObservacaoMotorista")] Rota rotaUpdate)
+        {
+            var rota = await _context.Rotas
+                .Include(r => r.Veiculo)
+                .Include(r => r.Funcionario)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (rota == null)
+            {
+                return NotFound();
+            }
+
+            rota.DataHoraChegada = DateTime.Now;
+            rota.FlDanos = rotaUpdate.FlDanos;
+            rota.ObservacaoMotorista = rotaUpdate.ObservacaoMotorista;
+
+            //if (ModelState.IsValid)
+            //{
+                try
+                {
+                    _context.Update(rota);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Rotas.Any(e => e.Id == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Details", new { id = rota.FuncionarioId });
+            //}
+
+            //return View(rota);
         }
     }
 }
